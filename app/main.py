d9fb78dd2821dd11e26ba701a5580ccf408e1186
lykhorovych <31163897+lykhorovych@users.noreply.github.com>
2025-12-27 10:47:53 +0200

    Resolve conflicts

--- app/main.py
+++ app/main.py
@@ -1,18 +1,20 @@
 from typing import Union
 
-
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
-        self.clean_mark_before_washing = self.clean_mark
 
 
 class CarWashStation:
     def __init__(
         self,
-        distance_from_city_center: int,
+        distance_from_city_center: float,
         clean_power: int,
         average_rating: float,
         count_of_ratings: int,
@@ -25,14 +27,15 @@ class CarWashStation:
     def serve_cars(self, cars: list[Car]) -> Union[int, float]:
         income = 0
         for car in cars:
-            if self.wash_single_car(car):
+            if self.clean_power >= car.clean_mark:
                 income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
         return income
 
     def calculate_washing_price(self, car: Car) -> float:
         costs = round(
             car.comfort_class
-            * (self.clean_power - car.clean_mark_before_washing)
+            * (self.clean_power - car.clean_mark)
             * self.average_rating
             / self.distance_from_city_center,
             1,
@@ -41,10 +44,7 @@ class CarWashStation:
 
     def wash_single_car(self, car: Car) -> bool:
         if self.clean_power > car.clean_mark:
-            car.clean_mark_before_washing = car.clean_mark
             car.clean_mark = self.clean_power
-            return True
-        return False
 
     def rate_service(self, rate: int) -> None:
         rating = self.average_rating * self.count_of_ratings + rate
